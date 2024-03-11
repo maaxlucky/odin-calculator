6a09b499f133e32fdc76b5f7891fab4769c2490a
maaxlucky <kenyangn00@gmail.com>
2024-03-11 19:53:29 +0200

    Added division

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const eraseBtn = document.querySelector('.erase')
 const plusBtn = document.querySelector('.plus')
 const minusBtn = document.querySelector('.minus')
 const asteriskBtn = document.querySelector('.asterisk')
+const divideBtn = document.querySelector('.divide')
 // const equalBtn = document.querySelector('.plus')
 
 let minusIsClicked = false
 let plusIsClicked = false
 let asteriskIsClicked = false
+let divideIsClicked = false
 
 operandButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -21,41 +23,58 @@ operandButtons.forEach((button) => {
         if (button.textContent === '=' && inputNum.textContent !== '' && plusIsClicked) {
             console.log(tempValues)
             plusBtn.style.backgroundColor = '#b95d00';
-            inputNum.textContent = parseInt(inputNum.textContent) + tempValues[tempValues.length - 1]
+            inputNum.textContent = parseFloat(inputNum.textContent) + tempValues[tempValues.length - 1]
         } else if (button.textContent === '=' && inputNum.textContent !== '' && minusIsClicked) {
             console.log(tempValues)
             minusBtn.style.backgroundColor = '#b95d00';
-            inputNum.textContent = tempValues[tempValues.length - 1] - parseInt(inputNum.textContent)
+            inputNum.textContent = tempValues[tempValues.length - 1] - parseFloat(inputNum.textContent)
         }
         else if (button.textContent === '=' && inputNum.textContent !== '' && asteriskIsClicked) {
             console.log(tempValues)
             asteriskBtn.style.backgroundColor = '#b95d00';
-            inputNum.textContent = tempValues[tempValues.length - 1] * parseInt(inputNum.textContent)
+            inputNum.textContent = tempValues[tempValues.length - 1] * parseFloat(inputNum.textContent)
+        }
+        else if (button.textContent === '=' && inputNum.textContent !== '' && divideIsClicked) {
+            console.log(tempValues)
+            divideBtn.style.backgroundColor = '#b95d00';
+            inputNum.textContent = Math.floor(tempValues[tempValues.length - 1] / parseFloat(inputNum.textContent) * 10) / 10
         }
         if (button.textContent === '+') {
-            tempValues.push(parseInt(inputNum.textContent))
+            tempValues.push(parseFloat(inputNum.textContent))
             inputNum.textContent = ''
             plusIsClicked = true
             minusIsClicked = false
             asteriskIsClicked = false
+            divideIsClicked = false
             plusBtn.style.backgroundColor = '#9a4e00';
         }
         if (button.textContent === '-') {
-            tempValues.push(parseInt(inputNum.textContent))
+            tempValues.push(parseFloat(inputNum.textContent))
             inputNum.textContent = ''
             minusIsClicked = true
             plusIsClicked = false
             asteriskIsClicked = false
+            divideIsClicked = false
             minusBtn.style.backgroundColor = '#9a4e00';
 
         }
         if (button.textContent === '*') {
-            tempValues.push(parseInt(inputNum.textContent))
+            tempValues.push(parseFloat(inputNum.textContent))
             inputNum.textContent = ''
             asteriskIsClicked = true
             plusIsClicked = false
             minusIsClicked = false
+            divideIsClicked = false
             asteriskBtn.style.backgroundColor = '#9a4e00';
+        }
+        if (button.textContent === '/') {
+            tempValues.push(parseFloat(inputNum.textContent))
+            inputNum.textContent = ''
+            divideIsClicked = true
+            asteriskIsClicked = false
+            plusIsClicked = false
+            minusIsClicked = false
+            divideBtn.style.backgroundColor = '#9a4e00';
         }
     })
 })
